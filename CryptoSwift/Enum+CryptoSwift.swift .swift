672abe5f8684aafe0c90eb0389e6309ff82aa06e
呂子揚 <yyxdev@gmail.com>
2020-04-21 15:42:26 +0800

    Enum+CryptoSwift.swift .swift

---
+++ CryptoSwift/Enum+CryptoSwift.swift .swift
@@ -0,0 +1,13 @@
+//
+//  Enum+CryptoSwift.swift
+//  Passqort
+//
+//  Created by 呂子揚 on 2020/4/13.
+//  Copyright © 2020 呂子揚. All rights reserved.
+//
+
+import Foundation
+enum CryptoError: Error {
+case EncryptError
+case DeCryptError 
+}
